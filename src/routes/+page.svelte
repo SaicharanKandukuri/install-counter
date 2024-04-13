c7c1e465b57573f60c86a99878de1fd0a5270148
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2024-04-13 09:46:55 +0530

    feat: better looking page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,22 +20,36 @@
 		}, intervel);
 	}
 
+	const installcmd = ". <(curl -Ls https://bit.ly/udroid-installer)"
+
 	onMount(() => {
 		counter(10, data.count.count, 8);
 	});
 </script>
 
-<div class="dark:bg-[#070704] bg-[#EDEDDE]">
-	<div class="flex flex-col h-screen items-center justify-center rounded-sm">
-		<div class="bg-[#4e6638] p-6 rounded-xl">
+<div class="h-screen">
+	<div class="flex flex-col h-[90%] items-center justify-center rounded-sm">
+		<div class="p-6 rounded-xl">
             <a
 			href="https://github.com/RandomCoderOrg/ubuntu-on-android"
 			target="_blank"
 			referrerpolicy="no-referrer"
-			class="text-[#070704] dark:text-[#ededde] text-4xl hover:underline dark:decoration-[#EDEDDE] text-bold font-mono"
+			class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
 			>UDROID Downloads</a
 		>
-		<p class="font-light text-center font-mono text-3xl text-[#070704] dark:text-[#ededde]">{finalText}</p>
+		<p class="transition-all mt-4 font-light text-center font-mono text-3xl text-[#070704] dark:text-[#ededde]">{finalText}</p>
         </div>
+
+		<blockquote class="mt-6 border-l-2 pl-6 italic text-[#070704] dark:text-[#ededde]">
+		  Install udroid 
+			<code
+			  class="relative rounded bg-muted px-[0.3rem] py-[0.2rem] w-60 font-mono text-sm font-semibold"
+			>
+			  {installcmd}
+			</code>
+		</blockquote>
 	</div>
+	<footer class="text-center w-full leading-7 [&:not(:first-child)]:mt-6 text-sm">
+		<a href="https://github.com/RandomCoderOrg/ubuntu-on-android/" target="_blank" rel="noopener noreferrer">RandomCoderOrg/ubuntu-on-android</a>
+	</footer>
 </div>
